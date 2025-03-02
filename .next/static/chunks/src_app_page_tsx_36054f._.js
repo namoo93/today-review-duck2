107d551b46b34795782cd934afb3133a9a79ceab
hyoseon <67184684+namoo93@users.noreply.github.com>
2025-03-02 17:20:58 +0900

    불필요한 파일 삭제

--- .next/static/chunks/src_app_page_tsx_36054f._.js
+++ .next/static/chunks/src_app_page_tsx_36054f._.js
@@ -5,9 +5,8 @@
 (globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
   "path": "static/chunks/src_app_page_tsx_36054f._.js",
   "chunks": [
-    "static/chunks/node_modules_next_dist_ea69ac._.js",
-    "static/chunks/src_app_page_tsx_8b213b._.js",
-    "static/chunks/src_app_page_module_d12eb7.css"
+    "static/chunks/src_app_page_module_d12eb7.css",
+    "static/chunks/src_app_page_tsx_8b213b._.js"
   ],
   "source": "dynamic"
 });
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,9 @@
-import Image from "next/image";
 import styles from "./page.module.css";
 
 export default function Home() {
   return (
     <div className={styles.page}>
-      <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-      </main>
+      <main className={styles.main}></main>
       <footer className={styles.footer}></footer>
     </div>
   );
