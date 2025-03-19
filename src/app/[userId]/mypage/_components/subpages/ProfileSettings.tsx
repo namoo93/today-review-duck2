0574379ace5688f6b7a86117d07a1775598b12fd
hyoseon <67184684+namoo93@users.noreply.github.com>
2025-03-20 01:09:29 +0900

    디렉토리명 변경

--- src/app/[userId]/mypage/_components/subpages/ProfileSettings.tsx
+++ src/app/[userId]/mypage/_components/subpages/ProfileSettings.tsx
@@ -1,10 +1,10 @@
 "use client";
 import styles from "../../_css/profilesettings.module.css";
 import Modal from "@/app/_components/modal/Modal";
-import ProfileForm from "./profileSettings/ProfileForm";
-import ProfileInfo from "./profileSettings/ProfileInfo";
-import ProfileImage from "./profileSettings/ProfileImage";
-import ProfileBottom from "./profileSettings/ProfileBottom";
+import ProfileForm from "./profile/ProfileForm";
+import ProfileInfo from "./profile/ProfileInfo";
+import ProfileImage from "./profile/ProfileImage";
+import ProfileBottom from "./profile/ProfileBottom";
 
 export default function ProfileSettings() {
   return (
