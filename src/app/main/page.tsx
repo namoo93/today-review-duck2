52e9cbe3580af3af57791778a48c10f77cefaae3
hyoseon <67184684+namoo93@users.noreply.github.com>
2025-03-31 00:42:00 +0900

    캐쉬제거후 재빌드

--- src/app/main/page.tsx
+++ src/app/main/page.tsx
@@ -3,8 +3,8 @@
 import { useRecoilState } from "recoil";
 import { onSearchPageState } from "../_recoil";
 import Banner from "./_components/Banner";
-import SearchList from "./_components/SearchList";
 import MainList from "./_components/MainList";
+import SearchList from "./_components/SearchList";
 import FixedButton from "../_components/fixedButton/FixedButton";
 
 export default function Main() {
