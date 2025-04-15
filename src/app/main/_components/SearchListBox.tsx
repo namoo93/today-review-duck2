a9c4015c04e04ef8805763ee0132b40306134901
hyoseon <67184684+namoo93@users.noreply.github.com>
2025-04-16 07:16:35 +0900

    검색후 유저 리스트 데이터 중간저장

--- src/app/main/_components/SearchListBox.tsx
+++ src/app/main/_components/SearchListBox.tsx
@@ -10,18 +10,34 @@ import { useSearchResult } from "@/app/_hooks/useSearchResult";
 import Pagination from "@/app/_components/pagination/Pagination";
 import DataNone from "@/app/_components/atoms/DataNone";
 import { searchKeywordState } from "@/app/_recoil/searchKeywordAtom";
+import ProfileBox from "@/app/_components/profile/ProfileBox";
+import { useToggleFollow } from "@/app/_hooks/useToggleFollow";
+import { useRouter } from "next/navigation";
 
 export default function SearchListBox() {
+  const router = useRouter();
   const [theme] = useRecoilState(themeState);
   const keyword = useRecoilValue(searchKeywordState);
   const [tab, setTab] = useState("게시글");
   const [currentPage, setCurrentPage] = useState(1);
 
   const { data, isLoading } = useSearchResult(tab, keyword, currentPage);
+  const { follow, unfollow } = useToggleFollow();
   const totalPages = data?.totalPage ?? 1;
   const isEmpty =
     (tab === "게시글" && data?.reviews.length === 0) ||
     (tab === "유저" && data?.users.length === 0);
+
+  const handleToggleFollow = (userIdx: string, isFollowing: boolean) => {
+    if (isFollowing) {
+      unfollow.mutate(userIdx);
+    } else {
+      follow.mutate(userIdx);
+    }
+  };
+  const goToUserPage = (user: string) => {
+    router.push(`/${user}`);
+  };
 
   return (
     <section className={styles.page}>
@@ -41,27 +57,43 @@ export default function SearchListBox() {
         <DataNone target={`검색된 키워드' ${keyword}' 의 ${tab}(이)`} />
       ) : (
         <>
-          <ul className={styles.list_wrap}>
-            {tab === "게시글"
-              ? data?.reviews.map((review: ReviewType) => (
-                  <List
-                    key={review.idx}
-                    isManager={false}
-                    alt={review.title}
-                    src={review.thumbnail}
-                    title={review.title}
-                    user={review.user}
-                    value={review.score}
-                    contents={review.content}
-                  />
-                ))
-              : data?.users.map((user: MyInfoType) => (
-                  <li key={user.idx}>
-                    {user.nickname} {/* 사용자 카드 대체 */}
-                  </li>
-                ))}
-          </ul>
+          {tab === "게시글" && (
+            <ul className={styles.list_wrap_user}>
+              {data?.reviews.map((review: ReviewType) => (
+                <List
+                  key={review.idx}
+                  isManager={false}
+                  alt={review.title}
+                  src={review.thumbnail}
+                  title={review.title}
+                  user={review.user}
+                  value={review.score}
+                  contents={review.content}
+                />
+              ))}
+            </ul>
+          )}
 
+          {tab === "유저" && (
+            <ul className={styles.list_wrap}>
+              {data?.users.map((user: MyInfoType) => (
+                <li key={`검색된 유저 리스트 ${user.nickname}`}>
+                  <ProfileBox
+                    name={user.nickname}
+                    onClickBox={() => goToUserPage(user.nickname)}
+                    interest={`${user.interest1} ${user.interest2}`}
+                    textWidth={"190px"}
+                    isOn={user.isMyFollowing}
+                    isOnText="덕질 중"
+                    isOffText="덕질하기"
+                    onClickButton={() =>
+                      handleToggleFollow(user.idx, user.isMyFollowing)
+                    }
+                  />
+                </li>
+              ))}
+            </ul>
+          )}
           {totalPages > 1 && (
             <Pagination
               currentPage={currentPage}
